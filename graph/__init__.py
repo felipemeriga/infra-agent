ebbe64a5bb8f3b8e7d684f00504311518d0b994e
Felipe Ramos da Silva <felipe.ramosdesilva@cosm.com>
2026-03-30 20:55:56 -0300

    chore: add auto_respond graph export

--- graph/__init__.py
+++ graph/__init__.py
@@ -1,8 +1,10 @@
+from graph.auto_respond import build_auto_respond_graph
 from graph.deploy import build_deploy_graph
 from graph.diagnose import build_diagnose_graph
 from graph.restart import build_restart_graph
 
 __all__ = [
+    "build_auto_respond_graph",
     "build_deploy_graph",
     "build_diagnose_graph",
     "build_restart_graph",
